5f0fe7c5ed6235d5dc74d2d592926c7f3fe5e874
Guruhg19 <guruhvitra19@gmail.com>
2025-04-27 19:57:37 +0700

    feat: Add course creation page and update navigation for course management
    - Created a new course creation page with a sidebar and course listing.
    - Updated the courses index page to include a button for creating new courses.
    - Enhanced the navigation layout to include a link for managing courses.

--- resources/views/admin/courses/index.blade.php
+++ resources/views/admin/courses/index.blade.php
@@ -1,1 +1,17 @@
-<p>Ini Halaman teacher</p>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
+                <a href="{{ route('dashboard.courses.create') }}" class="px-5 py-4 text-white bg-indigo-600 rounded-full">
+                    Create New Courses
+                </a>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
